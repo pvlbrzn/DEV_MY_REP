b238237cd9c6a9a75825004124d59bea6922c525
pvlbrzn <pavelberezan1998@gmail.com>
2025-01-08 22:09:57 +0300

    add lesson_15

---
+++ lesson_15/task_1.py
@@ -0,0 +1,87 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String, ForeignKey
+)
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+Base = declarative_base()
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    books = relationship("Book", back_populates="author")
+
+
+# Таблица books
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    publication_year = Column(Integer)
+    author = relationship("Author", back_populates="books")
+    sales = relationship("Sale", back_populates="book")
+
+
+# Таблица sales
+class Sale(Base):
+    __tablename__ = 'sales'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, ForeignKey('books.id'))
+    quantity = Column(Integer, nullable=False)
+    book = relationship("Book", back_populates="sales")
+
+
+# Создаем базу данных
+engine = create_engine('sqlite:///library.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# Заполнение данных
+def populate_data():
+    # Добавляем авторов
+    author1 = Author(first_name="Джордж", last_name="Оруэлл")
+    author2 = Author(first_name="Рэй", last_name="Брэдбари")
+    author3 = Author(first_name="Антон", last_name="Чехов")
+    session.add_all([author1, author2, author3])
+    session.commit()
+
+    # Добавляем книги
+    book1 = Book(title="Скотный двор", author_id=author1.id, publication_year=1869)
+    book2 = Book(title="451 градус по Фарингейту", author_id=author2.id, publication_year=1953)
+    book3 = Book(title="Преступление и наказание", author_id=author3.id, publication_year=1866)
+    book4 = Book(title="1984", author_id=author1.id, publication_year=1948)
+    session.add_all([book1, book2, book3, book4])
+    session.commit()
+
+    # Добавляем продажи
+    sale1 = Sale(book_id=book1.id, quantity=150)
+    sale2 = Sale(book_id=book2.id, quantity=120)
+    sale3 = Sale(book_id=book3.id, quantity=200)
+    sale4 = Sale(book_id=book4.id, quantity=180)
+    session.add_all([sale1, sale2, sale3, sale4])
+    session.commit()
+
+
+populate_data()
+
+# INNER JOIN для получения списка всех книг и их авторов
+inner_join_books_authors = session.query(Book.title, Author.first_name, Author.last_name).join(Author).all()
+
+# LEFT JOIN для списка всех авторов и их книг
+left_join_authors_books = session.query(Author.first_name, Author.last_name, Book.title).outerjoin(Book).all()
+
+# RIGHT JOIN для списка всех книг и их авторов
+right_join_books_authors = session.query(Book.title, Author.first_name, Author.last_name).outerjoin(Author).all()
+
+
+print("INNER JOIN (Books and Authors):", inner_join_books_authors)
+print("LEFT JOIN (Authors and Books):", left_join_authors_books)
+print("RIGHT JOIN (Books and Authors):", right_join_books_authors)
